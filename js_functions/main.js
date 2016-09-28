eefd62024d593b95c6a9ad0d8934201e8afc4add
Kevin Chau <kchau.jobs@gmail.com>
2016-09-28 11:24:29 -0700

    js functions - Kevin Chau

---
+++ js_functions/main.js
@@ -0,0 +1,13 @@
+function myMessage(){
+    console.log("Hello, World!");
+}
+
+function add2(num1, num2){
+    return num1 + num2;
+}
+
+var add2Result = add2(10, 36);
+
+function cardFlip(element){
+    $(element).hide();
+}
