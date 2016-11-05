c21aa86297a9e49def746205e2e1938bf6f145b9
Kevin Chau <kchau.jobs@gmail.com>
2016-11-04 18:01:16 -0700

    regex works

--- php_sessions/session_reader.php
+++ php_sessions/session_reader.php
@@ -1,1 +1,23 @@
 <!-- Session Reader -->
+<?php
+session_start();
+print_r($_GET);
+$error = array();
+if(preg_match('/\d/', $_GET['user_name'])){
+    array_push($error, 'Invalid Name Entered');
+}
+if(preg_match('/\D/', $_GET['user_age'])){
+    array_push($error, 'Invalid Age Entered');
+}
+if(preg_match('/\d/', $_GET['user_occupation'])){
+    array_push($error, 'Invalid Occupation Entered');
+}
+if(count($error) > 0){
+    $_SESSION['errors_found'] = $error;
+    header('location: session_setter.php');
+}
+
+$_SESSION['my_name'] = $_GET['user_name'];
+$_SESSION['my_age']= $_GET['user_age'];
+$_SESSION['my_occupation'] = $_GET['user_occupation'];
+?>
--- php_sessions/session_setter.php
+++ php_sessions/session_setter.php
@@ -1,1 +1,20 @@
 <!-- Session Setter -->
+<?php
+session_start();
+?>
+<form action="session_reader.php">
+    Name:
+    <br><input type="text" name="user_name"
+               value="<?php echo isset($_SESSION['my_name'])?$_SESSION['my_name']:''; ?>"><br>
+    Age:
+    <br><input type="text" name="user_age"
+               value="<?php echo isset($_SESSION['my_age'])?$_SESSION['my_age']:''; ?>"><br>
+    Occupation:
+    <br><input type="text" name="user_occupation"
+               value="<?php echo isset($_SESSION['my_occupation'])?$_SESSION['my_occupation']:''; ?>"><br>
+    <button>Submit</button>
+</form>
+
+<?php
+print_r($_SESSION);
+?>
