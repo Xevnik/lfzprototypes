117bee26855ee5c57e3e67bdab77753c8aa24ee6
Kevin Chau <kchau.jobs@gmail.com>
2016-11-08 09:32:10 -0800

    no drastic changes

--- php_sessions/session_reader.php
+++ php_sessions/session_reader.php
@@ -3,6 +3,7 @@
 session_start();
 print_r($_GET);
 $error = array();
+
 if(preg_match('/\d/', $_GET['user_name'])){
     array_push($error, 'Invalid Name Entered');
 }
@@ -13,11 +14,9 @@ if(preg_match('/\d/', $_GET['user_occupation'])){
     array_push($error, 'Invalid Occupation Entered');
 }
 if(count($error) > 0){
-    $_SESSION['errors_found'] = $error;
+    $_SESSION['errors'] = $error;
     header('location: session_setter.php');
 }
+$_SESSION = $_GET;
 
-$_SESSION['my_name'] = $_GET['user_name'];
-$_SESSION['my_age']= $_GET['user_age'];
-$_SESSION['my_occupation'] = $_GET['user_occupation'];
 ?>
--- php_sessions/session_setter.php
+++ php_sessions/session_setter.php
@@ -5,13 +5,13 @@ session_start();
 <form action="session_reader.php">
     Name:
     <br><input type="text" name="user_name"
-               value="<?php echo isset($_SESSION['my_name'])?$_SESSION['my_name']:''; ?>"><br>
+               value="<?php echo isset($_SESSION['user_name'])?$_SESSION['user_name']:''; ?>"><br>
     Age:
     <br><input type="text" name="user_age"
-               value="<?php echo isset($_SESSION['my_age'])?$_SESSION['my_age']:''; ?>"><br>
+               value="<?php echo isset($_SESSION['user_age'])?$_SESSION['user_age']:''; ?>"><br>
     Occupation:
     <br><input type="text" name="user_occupation"
-               value="<?php echo isset($_SESSION['my_occupation'])?$_SESSION['my_occupation']:''; ?>"><br>
+               value="<?php echo isset($_SESSION['user_occupation'])?$_SESSION['user_occupation']:''; ?>"><br>
     <button>Submit</button>
 </form>
 
