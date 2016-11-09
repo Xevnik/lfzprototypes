8ed0b5c4454d58a8b3946fe0ff773ba8902d7a1d
Kevin Chau <kchau.jobs@gmail.com>
2016-11-09 09:02:07 -0800

    PHP Sessions - Kevin Chau

--- php_sessions/session_reader.php
+++ php_sessions/session_reader.php
@@ -3,19 +3,19 @@
 <?php
 session_start();
 $_SESSION = $_GET;
+print_r($_SESSION);
 $_SESSION['errors'] = array();
 if(preg_match('/\d/', $_GET['user_name'])){
-    array_push($_SESSION['errors'], array("name"=>"Invalid Name entered"));
+    $_SESSION['errors']['name'] = "Invalid Name entered";
 }
 if(preg_match('/\D/', $_GET['user_age'])){
-    array_push($_SESSION['errors'], array('age'=>'Invalid Age Entered'));
+    $_SESSION['errors']['age'] = 'Invalid Age Entered';
 }
 if(preg_match('/\d/', $_GET['user_occupation'])){
-    array_push($_SESSION['errors'], array('occupation'=>'Invalid Occupation Entered'));
+    $_SESSION['errors']['occupation'] = 'Invalid Occupation Entered';
 }
 if(count($_SESSION['errors']) > 0){
-    print_r($_SESSION);
-    //header('location: session_setter.php');
+    header('location: session_setter.php');
 }
 ?>
 </pre>
--- php_sessions/session_setter.php
+++ php_sessions/session_setter.php
@@ -1,23 +1,25 @@
 <!-- Session Setter -->
-<pre>
-    <?php
-    session_start();
-    ?>
-    <form action="session_reader.php">
+<?php
+session_start();
+?>
+<form action="session_reader.php">
     Name:
     <br><input type="text" name="user_name"
                value="<?php echo isset($_SESSION['user_name'])?$_SESSION['user_name']:''; ?>"><br>
+    <?php echo isset($_SESSION['errors']['name'])?$_SESSION['errors']['name'].'<br>':''; ?>
     Age:
     <br><input type="text" name="user_age"
                value="<?php echo isset($_SESSION['user_age'])?$_SESSION['user_age']:''; ?>"><br>
+    <?php echo isset($_SESSION['errors']['age'])?$_SESSION['errors']['age'].'<br>':''; ?>
     Occupation:
     <br><input type="text" name="user_occupation"
                value="<?php echo isset($_SESSION['user_occupation'])?$_SESSION['user_occupation']:''; ?>"><br>
+    <?php echo isset($_SESSION['errors']['occupation'])?$_SESSION['errors']['occupation'].'<br>':''; ?>
+
     <button>Submit</button>
 </form>
-
+<pre>
     <?php
-    print_r($_SESSION);
-    var_dump($_SESSION);
+    //print_r($_SESSION);
     ?>
 </pre>
