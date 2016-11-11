c96f051b513c3572bc39a4df1524f51bd778c6fb
Kevin Chau <kchau.jobs@gmail.com>
2016-11-10 17:12:57 -0800

    carousel

--- php_directory_operations/carousel.php
+++ php_directory_operations/carousel.php
@@ -5,3 +5,87 @@
  * Date: 11/10/16
  * Time: 9:59 AM
  */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+        .carousel-inner > .item > img,
+        .carousel-inner > .item > a > img {
+            width: 70%;
+            margin: auto;
+        }
+        img{
+            width: 100%;
+            position: absolute;
+            left: 0;
+            top: 0;
+        }
+        .image_container{
+            position: relative;
+            width: 800px;
+            height: 600px;
+            left:50%;
+            transform: translateX(-50%);
+        }
+        button{
+            position: absolute;
+            z-index: 1;
+        }
+        #prev{
+            left: 0;
+        }
+        #next{
+            right: 0;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="image_container">
+        <button id='prev' class="btn btn-info" onclick="prev_image()"> < </button>
+        <button id='next' class="btn btn-info" onclick="next_image()"> > </button>
+    </div>
+</div>
+<script>
+    var image_array = [];
+    var activeImg = 0;
+    $(document).ready(function(){
+        load_files();
+    });
+    function initialize(){
+
+    }
+    function load_files(){
+        $.ajax({
+            method: 'GET',
+            url: 'get_images.php',
+            dataType: 'JSON',
+            success: function(resp){
+                if(resp.success){
+                    console.log('ajax call ', resp);
+                    for (var img in resp.files){
+                        var imgSrc = resp.files[img];
+                        console.log(imgSrc);
+                        image_array.push(imgSrc);
+                        var $img = $('<img>',{
+                            src: imgSrc
+                        });
+                        $('.image_container').append($img);
+                    }
+                    console.log('Image Array ', image_array);
+                }
+            }
+        });
+    }
+</script>
+</body>
+</html>
+
+
