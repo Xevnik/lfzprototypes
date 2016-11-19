a6919502daa2380c91ad437484a0911f81e7b2aa
Kevin Chau <kchau.jobs@gmail.com>
2016-11-19 15:59:34 -0800

    PHP User Auth - Kevin Chau

--- php_user_auth_basic/login_form.php
+++ php_user_auth_basic/login_form.php
@@ -1,1 +1,40 @@
 <!--login_form.php-->
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>User Auth</title>
+    <script src="https://code.jquery.com/jquery-2.2.4.js"></script>
+</head>
+<body>
+<input type="text" name="username" id="username" placeholder="Username"><br>
+<input type="password" name="userPassword" id="userPass" placeholder="Password"><br>
+<button type="button">Login</button>
+<span></span>
+</body>
+<script>
+    $(document).ready(function(){
+        $('button').click(sendLogin);
+    });
+
+    function sendLogin() {
+        var dataObj = {
+            'username': $('#username').val(),
+            'password': $('#userPass').val()
+        };
+
+        $.ajax({
+            url: 'login_handler.php',
+            data: dataObj,
+            cache: false,
+            method: 'post',
+            dataType: "json",
+            success: function (resp) {
+                console.log('Server Response: ', resp);
+                $('span').text(resp.message);
+            }
+        });
+    }
+</script>
+</html>
--- php_user_auth_basic/login_handler.php
+++ php_user_auth_basic/login_handler.php
@@ -1,1 +1,31 @@
-<!--login_handler.php-->
+<?php
+$user_info = [
+    ['id'=>1, 'username'=>'kchau', "password"=>"bd771e67ad4a69d9408bcc2b9614f2c046ca5d24"],
+    ['id'=>2, 'username'=>'redranger', "password"=>"e06d26ada448bc45a0b9652e1ebddecc07a126cb"],
+    ['id'=>3, 'username'=>'fetchexpert', "password"=>"e669fc9d9c5d956f00e7df513e9ac7f5b4abae26"],
+    ['id'=>4, 'username'=>'kingarthur', "password"=>"611c366a71c5a51452b4979cb5086a4be972d2b5"],
+    ['id'=>5, 'username'=>'gandalf', "password"=>"84a0d67b46778b2a67ee88acf427975ba2a6be00"]
+];
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$success = false;
+$message = 'Access Denied';
+$output= [];
+
+foreach($user_info as $user){
+    if($username === $user['username'] && sha1($password) === $user['password']){
+        $message = ('User Credentials Accepted');
+        $_SESSION['user_id'] = $user['id'];
+        $output['user_id'] = $user['id'];
+        $success = true;
+    }else{
+        $message = ('Username and password do not match');
+    }
+}
+$output['success'] = $success;
+$output['message'] = $message;
+
+$outputString = json_encode($output);
+print($outputString);
+?>
