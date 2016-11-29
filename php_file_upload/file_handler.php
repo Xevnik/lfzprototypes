71e6348f6fb6bc2908b6c2ae53773d750ccd9e8b
Kevin Chau <kchau.jobs@gmail.com>
2016-11-28 19:15:03 -0800

    did stuff

--- php_file_upload/file_handler.php
+++ php_file_upload/file_handler.php
@@ -1,1 +1,7 @@
 <!-- File Handler -->
+<pre>
+<?php
+print_r($_FILES);
+print_r($_POST);
+?>
+</pre>
--- php_file_upload/uploader_form.php
+++ php_file_upload/uploader_form.php
@@ -3,8 +3,10 @@
 <head>
 </head>
 <body>
-	<form id="file_upload">
-		
+	<form enctype="multipart/form-data" id="file_upload" method="post" action="file_handler.php">
+		<input type="file" name="upload_file" id="fileToUpload"><br>
+		<input type="text" name="description" id="fileDescription" placeholder="File Description"><br>
+		<button type="submit">Submit</button>
 	</form>
 </body>
 </html>
