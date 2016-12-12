5dd6f72d1ccc48c8221eb29fbbab8df9a80d14ad
Kevin Chau <kchau.jobs@gmail.com>
2016-12-11 18:55:32 -0800

    permission problems

--- php_file_upload/file_handler.php
+++ php_file_upload/file_handler.php
@@ -1,7 +1,21 @@
 <!-- File Handler -->
 <pre>
 <?php
+//declare variables
+$target_dir = __DIR__ . "/uploads";
+$target_file = $target_dir . $_FILES["upload_file"]["name"];
+$uploadOk = true;
+$output = [];
+print($target_dir);
+
+if(file_exists($target_file)){
+    $fileSplit = pathinfo($target_file);
+    print_r($fileSplit);
+}else{
+    die("\nDirectory does not exist");
+}
 print_r($_FILES);
 print_r($_POST);
+print_r($target_file);
 ?>
 </pre>
